d9530b46011859cac3064a7feef665d08d2e29f5
John Braun <jbraunnl@gmail.com>
2019-04-01 08:36:15 +0200

    Add more style fix rules

--- src/Fixer.php
+++ src/Fixer.php
@@ -12,17 +12,43 @@ class Fixer
     {
         $this->text = $text;
         $this->separateUnitsFromQuantity();
+        $this->useTheCorrectMinusSymbol();
+        $this->removeSpacesBetweenMinusAndQuantity();
     }
 
     protected function separateUnitsFromQuantity()
     {
-        $pattern = '/(\d+\.?\d*)([a-zA-Z]+\/?[a-zA-Z]*)/';
+        $pattern = '/(\d+\.?\d*)(°?[a-zA-Z]+\/?[a-zA-Z]*)/u';
 
         preg_match_all($pattern, $this->text, $matches, PREG_SET_ORDER);
 
         foreach ($matches as $match) {
             $this->errors[] = $match[0];
             $this->corrections[$match[0]] = "{$match[1]} {$match[2]}";
+        }
+    }
+
+    protected function useTheCorrectMinusSymbol()
+    {
+        $correctMinus = '−';
+        $pattern = '/((-\s*(\d+\.?\d*))\s*(°?[a-zA-Z]+\/?[a-zA-Z]*))/u';
+
+        preg_match_all($pattern, $this->text, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $this->errors[] = $match[0];
+            $this->corrections[$match[0]] = "$correctMinus{$match[3]} {$match[4]}";
+        }
+    }
+
+    protected function removeSpacesBetweenMinusAndQuantity()
+    {
+        $pattern = '/((−\s+(\d+\.?\d*))\s*(°?[a-zA-Z]+\/?[a-zA-Z]*))/u';
+        preg_match_all($pattern, $this->text, $matches, PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $this->errors[] = $match[0];
+            $this->corrections[$match[0]] = "−{$match[3]} {$match[4]}";
         }
     }
 }
--- tests/FixerTest.php
+++ tests/FixerTest.php
@@ -11,17 +11,57 @@ class FixerTest extends TestCase
     public function separates_quantity_from_unit()
     {
         $fixer = new Fixer(
-            'A solution of A (15mL) was added to 3g something else...'
+            'A solution of A (15mL, 2 mmol, 1.0equiv) was added to 3g something else...'
         );
 
         $expectedCorrections = [
             '15mL'  => '15 mL',
+            '1.0equiv' => '1.0 equiv',
             '3g'    => '3 g',
         ];
 
-        $this->assertCount(2, $fixer->errors);
+        $this->assertCount(3, $fixer->errors);
         $this->assertEquals('15mL', $fixer->errors[0]);
-        $this->assertEquals('3g', $fixer->errors[1]);
+        $this->assertEquals('1.0equiv', $fixer->errors[1]);
+        $this->assertEquals('3g', $fixer->errors[2]);
         $this->assertEquals($expectedCorrections, $fixer->corrections);
+    }
+
+    /** @test **/
+    public function units_with_a_degree_sign_are_separated_from_the_quantity()
+    {
+        $fixer = new Fixer('The reaction mixture was stirred at 20°C');
+
+        $this->assertCount(1, $fixer->errors);
+        $this->assertEquals('20°C', $fixer->errors[0]);
+        $this->assertEquals('20 °C', $fixer->corrections['20°C']);
+    }
+
+    /** @test **/
+    public function negative_values_have_the_correct_minus_symbol()
+    {
+        $text = 'The reaction mixture was stirred at -78 °C';
+        $fixer = new Fixer($text);
+
+        $this->assertCount(1, $fixer->errors);
+        $this->assertEquals('-78 °C', $fixer->errors[0]);
+        $this->assertEquals('−78 °C', $fixer->corrections['-78 °C']);
+    }
+
+    /** @test **/
+    public function spaces_between_minus_and_negative_values_are_removed()
+    {
+        $fixer = new Fixer('The reaction mixture was stirred at − 10 °C');
+
+        $this->assertCount(1, $fixer->errors);
+        $this->assertEquals('− 10 °C', $fixer->errors[0]);
+        $this->assertEquals('−10 °C', $fixer->corrections['− 10 °C']);
+
+        // Test with the 'wrong' minus symbol
+        $fixer = new Fixer('The reaction mixture was stirred at - 10 °C');
+
+        $this->assertCount(1, $fixer->errors);
+        $this->assertEquals('- 10 °C', $fixer->errors[0]);
+        $this->assertEquals('−10 °C', $fixer->corrections['- 10 °C']);
     }
 }
